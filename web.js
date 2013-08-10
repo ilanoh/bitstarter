af32a22574a9ff91b36d55869d167fa27db09432
Ubuntu <ubuntu@ip-172-31-30-13.us-west-2.compute.internal>
2013-08-10 15:44:46 +0000

    html and port 8080

--- web.js
+++ web.js
@@ -5,10 +5,11 @@ var htmlfile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
     response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
